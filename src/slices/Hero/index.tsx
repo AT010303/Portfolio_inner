509097b4227ffe5ad65028b80027e5f668b245f9
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-04-17 23:54:44 +0530

    added bounding box to hero

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useEffect, useRef } from "react";
 
@@ -6,6 +6,7 @@ import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
 import gsap from "gsap";
+import Bounded from "@/app/components/Bounded";
 
 /**
  * Props for `Hero`.
@@ -16,54 +17,63 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
-
   const component = useRef(null);
 
-  useEffect(()=>{
+  useEffect(() => {
     let ctx = gsap.context(() => {
       const tl = gsap.timeline();
 
-      tl.fromTo(".name-animation", {
-        x: -100, opacity: 0, rotate: -10
-      },
-      {
-        x:0, opacity: 1, rotate: 0, ease: "elastic.out(1,0.3", duration: 1, transformOrigin: "left top", delay: 0.5, stagger: { each: 0.1, from: "random"}
-      }
-    );
+      tl.fromTo(
+        ".name-animation",
+        {
+          x: -100,
+          opacity: 0,
+          rotate: -10,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3",
+          duration: 1,
+          transformOrigin: "left top",
+          delay: 0.5,
+          stagger: { each: 0.1, from: "random" },
+        }
+      );
 
-    tl.fromTo(".job-title",
-      {
-        y: 20,
-        opacity: 0,
-        scale: 1.2
-      },
-      {
-        y: 0,
-        opacity: 1,
-        scale: 1,
-        ease: "elastic.out(1,0.3)",
-      }
-    )
-
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          scale: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
     }, component);
-    return ()=> ctx.revert();
+    return () => ctx.revert();
   }, []);
 
-  const renderLetters = (name:KeyTextField, key:string) => {
-    if(!name) return;
+  const renderLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
     return name.split("").map((letter, index) => (
       <span
-      key={index}
-      className={`name-animation name-animation-${key} inline-block opacity-0`}
+        key={index}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
       >
         {letter}
       </span>
     ));
   };
 
-
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
@@ -77,10 +87,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             }
           >
             <span className="block text-slate-300">
-              {renderLetters (slice.primary.first_name, "first")}
+              {renderLetters(slice.primary.first_name, "first")}
             </span>
             <span className="-mt-[.2em] block text-slate-500">
-              {renderLetters (slice.primary.last_name, "last")}
+              {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
           <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl">
@@ -88,7 +98,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
           </span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
