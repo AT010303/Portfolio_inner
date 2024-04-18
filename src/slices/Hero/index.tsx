9a06a104401bdb436eb1d9e80203e01c69768c05
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-04-18 11:37:18 +0530

    added a geometry

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,8 +5,9 @@ import { useEffect, useRef } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
-import gsap from "gsap";
+import { gsap } from "gsap";
 import Bounded from "@/app/components/Bounded";
+import Shape from "./Shape";
 
 /**
  * Props for `Hero`.
@@ -79,6 +80,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shape />
         <div className="col-start-1 md:row-start-1">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
