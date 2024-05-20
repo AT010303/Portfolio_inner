d2533381ff930dd98dc20fc3d39231a45c129236
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-05-20 23:44:56 +0530

    fixed project not working just name correction

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -31,7 +31,7 @@ export default function ContentList({
 
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blog" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
 
   useEffect(() => {
     // Animate list-items in with a stagger
