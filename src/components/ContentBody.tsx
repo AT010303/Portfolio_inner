ee2e8ceebf01b0256973a5ca48210e8384e096ea
AT010303 <101562874+AT010303@users.noreply.github.com>
2024-05-28 12:09:33 +0530

    fixed tag hight

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -16,14 +16,14 @@ export default function ContentBody({
     <Bounded as="article">
       <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
-        <div className="flex gap-4 text-yellow-400">
+        <div className=" mt-5 flex gap-4 text-yellow-400">
           {page.tags.map((tag, index) => (
             <span key={index} className="text-xl font-bold">
               {tag}
             </span>
           ))}
         </div>
-        <p className="mt-8 border-b border-slate-600 text-xl font-medium text-slate-300">
+        <p className="mt-6 border-b border-slate-600 text-xl font-medium text-slate-300">
           {formattedDate}
         </p>
         <div className="prose prose-lg prose-invert mt-12 w-full max-w-none md:mt-20">
